deb6b1fe1d9d16de2c7ba3de304036c280efef06
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-12 13:15:14 -0600

    Creacion del Modelo

---
+++ Web-API-Controladores-1/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace Web_API_Controladores_1.Models
+{
+    public class Persona
+    {
+        // ATRIBUTOS:
+        public int IdPersona { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Residencia { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
